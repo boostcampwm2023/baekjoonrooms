700ec9667cf40301387095ea9a892da1e3e65029
sjn0910 <sjn0910@naver.com>
2023-11-21 13:37:27 +0900

    feat: typeorm module 설치

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true, // production시 false로 변경
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
