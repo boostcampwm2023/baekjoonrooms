7ddd235b26cab7e9bdd43a87a334624e52bfe979
kiuuon <k41471804@gmail.com>
2023-11-26 15:53:04 +0900

    style: tailwind color config 설정

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,9 +3,9 @@ import { useAuthUpdateContext } from '../contexts/AuthContext';
 export default function Home() {
   const { onLogin } = useAuthUpdateContext();
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-green-400/50 to-blue-500/50">
-      <h1 className="my-2 text-3xl font-bold">BOJ Rooms</h1>
-      <button className="my-2 flex items-center rounded-lg bg-gray-800 px-5 py-2.5 text-sm text-white hover:bg-gray-600">
+    <div className="bg-aod_bg flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-aod_text my-2 text-3xl font-bold">BOJ Rooms</h1>
+      <button className="text-aod_text my-2 flex items-center rounded-lg bg-gray-800 px-5 py-2.5 text-sm hover:bg-gray-600">
         <img
           src="/assets/Github.png"
           alt="GitHub logo"
