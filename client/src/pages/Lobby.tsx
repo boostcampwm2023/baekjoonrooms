e3aaf9b266f1f800d5c2762d38a2706e70a2c7af
dkubnt <kimdhyungg@gmail.com>
2023-11-26 17:28:12 +0900

    for frontend test

--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -2,15 +2,28 @@ import LogoutButton from '../components/buttons/LogoutButton';
 import Profile from '../components/Profile';
 import RoomAccessPanel from '../components/RoomAccessPanel';
 
+import axios from 'axios';
+
 export default function Lobby() {
+  async function testApi(): Promise<void> {
+    const response = await axios.get(`/`);
+    console.log(response);
+  }
+
   return (
-    <div className="bg-aod_bg min-h-screen">
+    <div className="min-h-screen bg-aod_bg">
       <div className="flex w-full justify-end">
         <LogoutButton />
       </div>
       <div className="mt-32 flex flex-col items-center">
         <Profile />
         <RoomAccessPanel />
+        <button
+          onClick={() => {
+            testApi();
+          }}>
+          testtest
+        </button>
       </div>
     </div>
   );
