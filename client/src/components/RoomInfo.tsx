2380682614c83f94794d8f379d3c60350e699f8d
Yechan Jung <asdxcv1998@gmail.com>
2023-11-26 17:08:53 +0900

    feat: png icon svg로 변경

--- client/src/components/RoomInfo.tsx
+++ client/src/components/RoomInfo.tsx
@@ -1,17 +1,19 @@
 import CreateRoom from '../../public/mocks/CreateRoom.json';
+import CopyIcon from '../icons/CopyIcon';
+import ExitIcon from '../icons/ExitIcon';
 
 export default function RoomInfo() {
   return (
     <div className="flex w-full items-center justify-between py-2.5">
-      <div className="flex flex-row items-center rounded-lg p-2">
-        <div className="pr-3 text-xl font-bold">{CreateRoom.code}</div>
-        <button className="rounded p-1">
-          <img src="../assets/copy.png" alt="copy" className="h-6 w-auto" />
-        </button>
+      <div className="flex flex-row items-center rounded-lg p-2 text-aod_text">
+        <div className="pr-3 text-xl font-bold">
+          {CreateRoom.code}
+        </div>
+        <CopyIcon />
       </div>
       <button className="flex flex-row items-center gap-x-2 rounded-lg p-2">
-        <img src="../assets/exit.png" alt="exit" className="h-6 w-auto" />
-        <div className="text-xl font-semibold">Exit</div>
+        <ExitIcon />
+        <div className="text-xl font-semibold text-aod_text">Exit</div>
       </button>
     </div>
   );
