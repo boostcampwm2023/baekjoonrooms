85935897650688ec8964d3b0695a5f39ca2be304
Seongwoo_Lukaid <crescent70004536@gmail.com>
2023-11-27 19:01:57 +0900

    refactor: svg -> react icons

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,14 +1,14 @@
-import SendIcon from '../icons/SendIcon';
+import { FaArrowRight } from 'react-icons/fa6';
 
 export default function Chat() {
   return (
     <div className="flex h-[250px] w-full flex-col justify-end">
-      <div className="bg-aod_white flex items-center justify-between gap-2 rounded-lg px-2.5 py-1.5">
+      <div className="flex items-center justify-between gap-2 rounded-lg bg-aod_white px-2.5 py-1.5">
         <input
           className="w-full text-sm outline-none"
           placeholder="텍스트를 입력하세요"
         />
-        <SendIcon />
+        <FaArrowRight />
       </div>
     </div>
   );
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,17 @@
 import { useAuthUpdateContext } from '../contexts/AuthContext';
+import { FaGithub } from 'react-icons/fa6';
 
 export default function Home() {
   const { onLogin } = useAuthUpdateContext();
   return (
-    <div className="bg-aod_bg flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-aod_text my-2 text-3xl font-bold">Baekjoon Rooms</h1>
-      <button className="text-aod_text bg-aod_black my-2 flex items-center rounded-lg px-5 py-2.5 text-sm hover:bg-gray-600">
-        <img
-          src="/assets/Github.png"
-          alt="GitHub logo"
-          className="mr-2 h-5 w-5"
-        />
+    <div className="flex min-h-screen flex-col items-center justify-center bg-aod_bg">
+      <h1 className="my-2 text-3xl font-bold text-aod_text">Baekjoon Rooms</h1>
+      <button className="hover:bg-gray-600 my-2 flex items-center gap-2 rounded-lg bg-aod_black px-4 py-2.5 text-sm text-aod_text">
+        <FaGithub size="1rem" />
         Login with GitHub
       </button>
       <button
-        className="my-2 rounded-lg bg-blue-500 px-5 py-2.5 text-sm font-bold text-white hover:bg-blue-700"
+        className="bg-blue-500 text-white hover:bg-blue-700 my-2 rounded-lg px-5 py-2.5 text-sm font-bold"
         onClick={() => {
           onLogin();
         }}>
