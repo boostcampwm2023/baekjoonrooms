3252488fceaa1df46fde9b78ff14dcf9f5d8ed7b
sjn0910 <sjn0910@naver.com>
2023-11-28 14:46:52 +0900

    feat: FindUserByProviderDto 제거

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,7 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from 'src/dtos/create.user.dto';
-import { FindUserByProviderDto } from 'src/dtos/find.user.by,provider.dto';
 import { Repository } from 'typeorm';
 import User from '../entities/user.entity';
 
@@ -14,16 +13,18 @@ export class UserService {
 
   async createUser(createUserDto: CreateUserDto) {
     const { provider, providerId } = createUserDto;
-    const findUserByProviderDto = { provider, providerId };
-    const user = await this.findUserByProviderInfo(findUserByProviderDto);
+    const user = await this.findUserByProviderInfo({ provider, providerId });
 
     if (user) throw new BadRequestException('이미 존재하는 유저입니다');
     return this.userRepository.create(createUserDto).save();
   }
 
-  async findUserByProviderInfo(findUserByProviderDto: FindUserByProviderDto) {
+  async findUserByProviderInfo(providerInfo: {
+    provider: string;
+    providerId: string;
+  }) {
     return this.userRepository.findOne({
-      where: findUserByProviderDto,
+      where: providerInfo,
     });
   }
 }
