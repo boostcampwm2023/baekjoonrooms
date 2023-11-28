fdf46a9f37a60ba16dc0d6e57d88a05dba72eb65
Yechan Jung <asdxcv1998@gmail.com>
2023-11-28 17:30:16 +0900

    refactor : Update ExitButton.tsx

--- client/src/components/buttons/ExitButton.tsx
+++ client/src/components/buttons/ExitButton.tsx
@@ -1,9 +1,12 @@
 import { RxExit } from 'react-icons/rx';
+import { useNavigate } from 'react-router-dom';
 
 export default function ExitButton() {
-  const exit =() => {
-    window.location.href = '/lobby';
-  }
+  const navigate = useNavigate();
+
+  const exit = () => {
+    navigate(`/lobby`);
+  };
   return (
     <button
       className="flex flex-row items-center gap-x-2 rounded-lg bg-aod_accent px-2.5 py-1 text-aod_white hover:opacity-80"
@@ -17,5 +20,3 @@ export default function ExitButton() {
     </button>
   );
 }
-
-
