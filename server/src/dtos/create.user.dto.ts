604f414f20ff323bbc583cff41aef8e87435d36e
sjn0910 <sjn0910@naver.com>
2023-11-28 09:07:27 +0900

    feat: create user swagger 적용 시도

--- server/src/dtos/create.user.dto.ts
+++ server/src/dtos/create.user.dto.ts
@@ -1,18 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'sjn0910',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    example: 'github',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   provider: string;
 
+  @ApiProperty({
+    example: '12341234',
+    required: true,
+  })
   @IsNumber()
   @IsNotEmpty()
   providerId: number;
 
+  @ApiProperty({
+    example: 'https://avatars.githubusercontent.com/u/71765155?v=4',
+    required: false,
+  })
   @IsString()
   @IsOptional()
   avatarUrl: string;
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/dtos/create.user.dto';
 import { UserService } from './user.service';
 
 @Controller('users')
+@ApiTags('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiOperation({
+    summary: '유저 생성',
+  })
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
   }
