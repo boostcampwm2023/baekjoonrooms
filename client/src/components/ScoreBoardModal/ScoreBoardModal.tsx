c6eb668ea1fa3c72fdf6156967bb88b9b3b74c6a
Yechan Jung <asdxcv1998@gmail.com>
2023-11-28 16:23:38 +0900

    feat: ScoreBoard 모달화

--- client/src/components/ScoreBoardModal/ScoreBoardModal.tsx
+++ client/src/components/ScoreBoardModal/ScoreBoardModal.tsx
@@ -1,7 +1,14 @@
 import { ResultType, ScoreType } from '../../types/ScoreType';
 import mockScoresData from '../../../public/mocks/Scores.json';
 import ScoreBoard from './ScoreBoard';
-import { FaChartSimple } from 'react-icons/fa6';
+import { FaChartSimple, FaXmark } from 'react-icons/fa6';
+import { RefObject } from 'react';
+
+interface ModalProps {
+  modalOverlayRef: RefObject<HTMLDivElement>;
+  closeModal: () => void;
+  modalOutsideClick: (arg: React.MouseEvent<HTMLDivElement>) => void;
+}
 
 const mockScores: ScoreType = {
   players: mockScoresData.players.map((player) => ({
@@ -12,18 +19,34 @@ const mockScores: ScoreType = {
   })),
 };
 
-export default function ScoreBoardModal() {
-  return (
-    <div className="flex h-[430px] w-[330px] flex-col items-center rounded-2xl bg-aod_bg">
-      <div className="flex w-full flex-col items-center gap-y-[2px] border-b-[0.5px] border-aod_gutter px-5 py-3">
-        <div className="flex flex-row items-baseline gap-2">
-          <FaChartSimple />
-          <div className="text-lg font-medium text-aod_text">Scoreboard</div>
-        </div>
-        <div className="text-xs text-aod_text">3 online</div>
-      </div>
+const iconStyle = {
+  fontSize: '1.5rem',
+};
 
-      <ScoreBoard scores={mockScores}></ScoreBoard>
+export default function ScoreBoardModal({
+  modalOverlayRef,
+  closeModal,
+  modalOutsideClick,
+}: ModalProps) {
+  return (
+    <div
+      className="absolute left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-aod_bg/80"
+      style={{ backdropFilter: 'blur(10px)' }}
+      ref={modalOverlayRef}
+      onClick={modalOutsideClick}>
+      <div className="relative flex h-[430px] w-[calc(50%-100px)] flex-col items-center rounded-2xl border-[0.5px] border-aod_gutter bg-aod_bg">
+        <button className="absolute top-4 right-4" onClick={closeModal}>
+          <FaXmark style={iconStyle} />
+        </button>
+        <div className="flex w-full flex-col items-center gap-y-[2px] border-b-[0.5px] border-aod_gutter px-5 py-3">
+          <div className="flex flex-row items-baseline gap-2">
+            <FaChartSimple />
+            <div className="text-lg font-medium text-aod_text">Scoreboard</div>
+          </div>
+          <div className="text-xs text-aod_text">3 online</div>
+        </div>
+        <ScoreBoard scores={mockScores}></ScoreBoard>
+      </div>
     </div>
   );
 }
--- client/src/components/buttons/ScoreBoardButton.tsx
+++ client/src/components/buttons/ScoreBoardButton.tsx
@@ -1,10 +1,32 @@
 import { FaChartSimple } from 'react-icons/fa6';
+import ScoreBoardModal from '../ScoreBoardModal/ScoreBoardModal';
+import { useRef, useState } from 'react';
 
 export default function ScoreboardButton() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const modalOverlayRef = useRef<HTMLDivElement>(null);
+  
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const modalOutsideClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (modalOverlayRef.current === event.target) {
+      setIsModalOpen(false);
+    }
+  };
   return (
-    <button className="flex w-full flex-row items-center justify-center rounded-lg bg-aod_accent px-3 py-2 text-aod_white hover:opacity-80">
-      <FaChartSimple />
-      <div className="pl-2 font-medium">Scoreboard</div>
-    </button>
+    <>
+      <button
+        className="flex w-full flex-row items-center justify-center rounded-lg bg-aod_accent px-3 py-2 text-aod_white hover:opacity-80"
+        onClick={openModal}>
+        <FaChartSimple />
+        <div className="pl-2 font-medium">Scoreboard</div>
+      </button>
+      {isModalOpen ? <ScoreBoardModal modalOverlayRef={modalOverlayRef} closeModal={closeModal} modalOutsideClick={modalOutsideClick}/> : <></>}
+    </>
   );
 }
