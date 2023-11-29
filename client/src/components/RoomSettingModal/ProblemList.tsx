13416cad57320412f414b4efdde76154ae4b54d9
Yechan Jung <asdxcv1998@gmail.com>
2023-11-29 18:29:08 +0900

    feat: custom dropdown, 시간 드롭다운

--- client/src/components/RoomSettingModal/ProblemList.tsx
+++ client/src/components/RoomSettingModal/ProblemList.tsx
@@ -14,7 +14,7 @@ export default function ProblemList({
   };
 
   return (
-    <div className="border-aod_black m-2 flex h-[250px] w-[250px] flex-col items-center rounded-lg border-2 p-4">
+    <div className="border-aod_gutter m-2 flex h-[250px] w-[250px] flex-col items-center rounded-lg border-2 p-4">
       {problemList.map((problem, index) => (
         <div className="flex w-full justify-between" key={index}>
           <div className="w-[150px] rounded-lg font-semibold">{problem}</div>
