1aadcb454cbdd4a9d67aecdda375e2d6bfd5e604
Yechan Jung <asdxcv1998@gmail.com>
2023-11-29 20:48:29 +0900

    feat: multiple dropdown 구현 2

--- client/src/components/RoomSettingModal/RandomProblem.tsx
+++ client/src/components/RoomSettingModal/RandomProblem.tsx
@@ -17,7 +17,7 @@ export default function RandomProblem() {
   return (
     <div className="flex flex-col items-center gap-2">
       <MultipleChoiceDropdown
-        options={['BFS', 'DFS', '에라토스테네스의 체', '구현','비트필드를 이용한 다이나믹 프로그래밍','커넥션 프로파일을 이용한 다이나믹 프로그래밍']}
+        options={['BFS', 'DFS', '에라토스테네스의 체', '구현','비트필드를 이용한 다이나믹 프로그래밍','커넥션 프로파일을 이용한 다이나믹 프로그래밍','A','B','C','D','E','F','G','H','I','J','K','L','M','N']}
         onOptionClick={onTagClick}
         buttonClassName="flex w-[232px] justify-center rounded-lg border border-aod_gutter bg-aod_white px-2 py-1 text-sm text-aod_black"
         itemBoxClassName="rounded-lg border border-aod_gutter"
