050c3cedc198ba5fc698fc775425c28fbdb18f51
dkubnt <kimdhyungg@gmail.com>
2023-11-29 14:51:48 +0900

    feat(client): 클라이언트 소켓 추가

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -5,10 +5,16 @@ import ScoreboardButton from '../components/buttons/ScoreBoardButton';
 import Chat from '../components/Chat';
 import StartButton from '../components/buttons/StartButton';
 import RoomInfo from '../components/RoomInfo';
+import { io } from 'socket.io-client';
 
 export default function Room() {
   const location = useLocation();
   const isHost = location.state?.isHost;
+
+  const socket = io('http://localhost:4000');
+  socket.on('connect', () => {
+    console.log('connected');
+  });
 
   return (
     <div className="relative flex min-h-screen items-center justify-center bg-aod_fg">
