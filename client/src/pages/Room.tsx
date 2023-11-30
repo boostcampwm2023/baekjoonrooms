b25ad1992dd787894108a27deeb859f96863b3ad
Yechan Jung <asdxcv1998@gmail.com>
2023-11-30 16:05:37 +0900

    feat: room 생성 플로우

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -9,11 +9,12 @@ import RoomInfo from '../components/RoomInfo';
 export default function Room() {
   const location = useLocation();
   const isHost = location.state?.isHost;
+  const roomCode = location.state?.roomCode;
 
   return (
     <div className="relative flex min-h-screen items-center justify-center bg-aod_fg">
       <div className="z-10 flex min-h-screen w-[50%] min-w-[300px] flex-col items-center gap-2 rounded-lg bg-aod_bg p-4 shadow-2xl">
-        <RoomInfo />
+        <RoomInfo roomCode={roomCode}/>
         <Problems isHost={isHost} />
         <ScoreboardButton />
         <Chat />
