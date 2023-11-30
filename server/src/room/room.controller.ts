fe2162cff04cd636d796f52e29f1573432d5f47f
sjn0910 <sjn0910@naver.com>
2023-11-30 10:55:57 +0900

    feat: post/create room 라우팅

--- server/src/room/room.controller.ts
+++ server/src/room/room.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RoomService } from './room.service';
+import { CreateRoomDto } from './dto/create.room.dto';
+import { UserService } from 'src/user/user.service';
 
 @Controller('room')
-export class RoomController {}
+export class RoomController {
+  constructor(
+    private readonly roomService: RoomService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Post()
+  async createRoom(@Body() createRoomDto: CreateRoomDto) {
+    return await this.roomService.createRoom(createRoomDto);
+  }
+}
--- server/src/room/room.module.ts
+++ server/src/room/room.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [RoomController],
-  providers: [RoomService]
+  providers: [RoomService],
 })
 export class RoomModule {}
