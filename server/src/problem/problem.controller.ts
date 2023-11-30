e7b7446efe1b643af93e70fe784d0fa116f7d59f
sjn0910 <sjn0910@naver.com>
2023-11-30 14:59:53 +0900

    feat: search problem 라우팅

--- server/src/problem/problem.controller.ts
+++ server/src/problem/problem.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ProblemService } from './problem.service';
+import { SearchProblemDto } from './dto/search.problem.dto';
 
 @Controller('problem')
-export class ProblemController {}
+export class ProblemController {
+  constructor(private readonly problemService: ProblemService) {}
+
+  @Get()
+  async searchProblem(@Query() searchProblemDto: SearchProblemDto) {
+    return this.problemService.searchProblem(searchProblemDto);
+  }
+}
