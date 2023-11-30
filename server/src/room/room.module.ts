6a321ca8aaea2393c0e32c7ef90905dc82e0c282
sjn0910 <sjn0910@naver.com>
2023-11-30 11:42:44 +0900

    feat: create room 서비스 생성

--- server/src/room/room.module.ts
+++ server/src/room/room.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
 import { UserModule } from 'src/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Room from 'src/entities/room.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TypeOrmModule.forFeature([Room])],
   controllers: [RoomController],
   providers: [RoomService],
 })
--- server/src/room/room.service.ts
+++ server/src/room/room.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import Room from 'src/entities/room.entity';
+import { Repository } from 'typeorm';
+import { CreateRoomDto } from './dto/create.room.dto';
+import { UserService } from 'src/user/user.service';
+import * as crypto from 'crypto';
 
 @Injectable()
-export class RoomService {}
+export class RoomService {
+  constructor(
+    @InjectRepository(Room)
+    private readonly roomRepository: Repository<Room>,
+    private readonly userService: UserService,
+  ) {}
+
+  async createRoom(createRoomDto: CreateRoomDto) {
+    const user = await this.userService.findUserWithRoomById(
+      createRoomDto.userId,
+    );
+    if (user.joinedRoom)
+      throw new BadRequestException('이미 방에 참가 중입니다.');
+
+    const roomCode = await this.createRoomCode(user.username);
+
+    return this.roomRepository
+      .create({ code: roomCode, host: user, users: [user] })
+      .save();
+  }
+
+  async createRoomCode(username: string) {
+    const currentTime = Date.now();
+    const hashSource = `${username}${currentTime}`;
+    const hash = crypto.createHash('sha256').update(hashSource).digest('hex');
+    return hash.substring(0, 6).toUpperCase();
+  }
+}
