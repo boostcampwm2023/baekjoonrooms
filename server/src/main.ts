5976ea2c3cfdade5f45d4f6dd89b8dbda62f2ace
dkubnt <kimdhyungg@gmail.com>
2023-12-01 03:58:14 +0900

    fix: minor: methods comma 위치 수정

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: 'http://localhost:5173',
-      methods: 'GET ,HEAD, PUT, PATCH, POST, DELETE',
+      methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
       allowedHeaders: 'Content-Type, Accept',
       credentials: true,
     },
