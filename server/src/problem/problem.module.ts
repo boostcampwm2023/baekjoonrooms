f1112249f19117290db9e4e29d002d9dc19ae66b
sjn0910 <sjn0910@naver.com>
2023-11-30 14:56:10 +0900

    feat: problem module 설정

--- server/src/problem/problem.module.ts
+++ server/src/problem/problem.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Problem from 'src/entities/problem.entity';
 import { ProblemController } from './problem.controller';
 import { ProblemService } from './problem.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Problem])],
   controllers: [ProblemController],
-  providers: [ProblemService]
+  providers: [ProblemService],
 })
 export class ProblemModule {}
