c7b108673f102f00b814693ab328ffb0a0e537f6
sjn0910 <sjn0910@naver.com>
2023-11-30 16:08:29 +0900

    feat: swagger 적용

--- server/src/problem/dto/search.problem.dto.ts
+++ server/src/problem/dto/search.problem.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class SearchProblemDto {
+  @ApiProperty({
+    example: 'A+B',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   searchKeyword: string;
--- server/src/problem/problem.controller.ts
+++ server/src/problem/problem.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { SearchProblemDto } from './dto/search.problem.dto';
 import { ProblemService } from './problem.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('problem')
+@ApiTags('problem')
 export class ProblemController {
   constructor(private readonly problemService: ProblemService) {}
 
   @Get()
+  @ApiOperation({
+    summary: '문제 검색',
+    description:
+      '백준 문제 번호 또는 문제 이름이 검색 키워드로 시작하는 모든 문제들을 검색합니다',
+  })
   async searchProblem(@Query() searchProblemDto: SearchProblemDto) {
     return this.problemService.searchProblem(searchProblemDto);
   }
