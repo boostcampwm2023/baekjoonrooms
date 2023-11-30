deae0887d5996d79f06d9b7932a2bc8db9d8be3e
dkubnt <kimdhyungg@gmail.com>
2023-12-01 03:57:34 +0900

    feat: :art: morgan 이 req, res를 양방향으로 멋지게 출력

--- server/src/main.ts
+++ server/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     },
   });
 
-  app.use(morgan('dev'));
+  morganSetup(app);
 
   app.use(cookieParser());
 
@@ -56,3 +56,40 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+function morganSetup(app) {
+  morgan.token('status-message', (req, res) => {
+    return res.statusMessage;
+  });
+
+  morgan.token('formatted-date', () => {
+    const date = new Date();
+    return date.toLocaleString('en-US', {
+      month: '2-digit',
+      day: '2-digit',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true,
+    });
+  });
+
+  app.use(
+    morgan(
+      '-->>> [:formatted-date] :remote-addr :remote-user ":method :url HTTP/:http-version"',
+      {
+        immediate: true,
+      },
+    ),
+  );
+  app.use(
+    morgan(
+      '<<<-- [:formatted-date] :status :status-message :response-time :res[content-length]',
+      {
+        immediate: false,
+      },
+    ),
+  );
+
+}
