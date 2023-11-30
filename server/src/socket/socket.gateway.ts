96aded37de150d37a01e6732acc52d9ff6c73a3b
dkubnt <kimdhyungg@gmail.com>
2023-11-30 16:21:07 +0900

    socket room 구현 중...

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -1,27 +1,60 @@
 import {
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+
+interface ChatResponse {
+  speaker: string;
+  message: string;
+}
 
 @WebSocketGateway({ cors: true })
-export class SocketGateway implements OnGatewayConnection {
+export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   private readonly server: Server;
 
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() data: string) {
-    console.log('sending message using WsResponse', data);
-
-    return { event: 'message', data: data };
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`client ${client.id} connected`);
+    client.emit('message', 'hello from server');
   }
 
-  // @SubscribeMessage('message')
-  // handleEvent(@MessageBody() data: string): string {
-  //   console.log('identity message:', data);
-  //   console.log('sending back the response!');
-  //   return 'naerisotnaersitonareistonareistnoarestionaresitnarsot';
-  // }
+  @SubscribeMessage('join-room')
+  handleJoinRoom(client: Socket, @MessageBody() data: { roomCode: string }) {
+    const { roomCode } = data;
+
+    console.log(
+      `handleJoinRoom [start]: ${client.id} joining the room ${roomCode}`,
+    );
+
+    const chatResponse: ChatResponse = {
+      speaker: 'server',
+      message: `Client ${client.id} joined the room ${roomCode}`,
+    };
+    client.join(roomCode);
+    this.server.to(roomCode).emit('chat-message', chatResponse);
+    console.log(
+      `handleJoinRoom [end]: ${client.id} joining the room ${roomCode}`,
+    );
+  }
+
+  @SubscribeMessage('chat-message')
+  handleMessage(client: Socket, @MessageBody() message: string) {
+    console.log(`handleMessage [start]: ${client.id} sent ${message}`);
+    const roomCode = 'rm1234';
+    const chatResponse: ChatResponse = {
+      speaker: client.id,
+      message: message,
+    };
+    this.server.to(roomCode).emit('chat-message', chatResponse);
+    console.log(`handleMessage [end]: ${client.id} sent ${message}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`client ${client.id} disconnected`);
+  }
 }
