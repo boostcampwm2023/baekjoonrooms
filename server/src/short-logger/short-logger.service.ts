68d92cb26e88efea0b2222d7272177912d3e568d
dkubnt <kimdhyungg@gmail.com>
2023-12-02 16:32:43 +0900

    feat(logger): 디버깅 친화적인 로깅

---
+++ server/src/short-logger/short-logger.service.ts
@@ -0,0 +1,40 @@
+import { ConsoleLogger, Injectable, LogLevel, Scope } from '@nestjs/common';
+
+@Injectable({ scope: Scope.TRANSIENT })
+export class ShortLoggerService extends ConsoleLogger {
+  protected getTimestamp(): string {
+    const now = new Date();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const date = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    return `${month}${date} ${hours}:${minutes}:${seconds}`;
+  }
+
+  protected formatPid(): string {
+    return '';
+  }
+
+  protected formatContext(context: string): string {
+    return super.formatContext(context.substring(0, 10));
+  }
+
+  protected formatMessage(
+    logLevel: LogLevel,
+    message: unknown,
+    pidMessage: string,
+    formattedLogLevel: string,
+    contextMessage: string,
+    timestampDiff: string,
+  ): string {
+    return super.formatMessage(
+      logLevel,
+      message,
+      pidMessage,
+      (logLevel as string).toUpperCase().substring(0, 3),
+      contextMessage,
+      timestampDiff,
+    );
+  }
+}
