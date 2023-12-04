9badfcc98e85707646cda89fd8e6f81445530741
dkubnt <kimdhyungg@gmail.com>
2023-12-04 13:00:06 +0900

    socket cors

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -9,31 +9,49 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessageInterface } from '../types/MessageInterface';
+import * as util from 'util';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({
+  cors: {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  },
+  transports: ['websocket', 'polling'],
+})
 export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   private readonly server: Server;
 
+  private readonly logger = new Logger(SocketGateway.name);
+
   handleConnection(@ConnectedSocket() client: Socket) {
-    console.log(`client ${client.id} connected`);
+    this.logger.debug(`client ${client.id} connected`);
     client.join('RM1234');
   }
 
   @SubscribeMessage('chat-message')
   handleMessage(
     @ConnectedSocket() client: Socket,
-    @MessageBody() message: MessageInterface,
+    @MessageBody() message: Partial<MessageInterface>,
   ) {
-    console.log(`handleMessage [start]: ${client.id} sent ${message.body}`);
+    this.logger.debug(`client ${client.id} sent ${util.inspect(message.body)}`);
 
-    const roomCode = 'RM1234';
-
-    this.server.to(roomCode).emit('chat-message', message);
-    console.log(`handleMessage [end]`);
+    this.logger.debug(
+      `<--- server sent ${util.inspect(message.body)} to room RM1234`,
+    );
+    this.server.to('RM1234').emit('chat-message', message);
   }
 
   handleDisconnect(@ConnectedSocket() client: Socket) {
-    console.log(`client ${client.id} disconnected`);
+    this.logger.debug(`client ${client.id} disconnected`);
+  }
+
+  @SubscribeMessage('piesocket-test')
+  piesocketTest(@MessageBody() body) {
+    this.logger.debug(`piesocket sent ${util.inspect(body)}`);
+    this.server
+      .to('RM1234')
+      .emit('piesocket-test', `${util.inspect(body)} from server!`);
   }
 }
