28c059aca987a7d9a73a5cc148cdcf9e8d01a704
dkubnt <kimdhyungg@gmail.com>
2023-12-05 05:38:11 +0900

    OAuth로 로그인한 유저를 createOrUpdateUser를 통해 1. 디비에 넣거나 2. 이미 존재하면 불러온다.

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -20,6 +20,19 @@ export class UserService {
     return this.userRepository.create(createUserDto).save();
   }
 
+  async createOrUpdateUser(createUserDto: CreateUserDto) {
+    const { provider, providerId } = createUserDto;
+    const user = await this.findUserByProviderInfo({ provider, providerId });
+
+    if (user) {
+      // TODO: check if user info is actually updated by the ORM
+      await this.userRepository.update(user.id, createUserDto);
+      return this.userRepository.findOneBy({ id: user.id });
+    } else {
+      return this.userRepository.create(createUserDto).save();
+    }
+  }
+
   async findUserByProviderInfo(providerInfo: ProviderInfo) {
     return this.userRepository.findOne({
       where: providerInfo,
