f7ab79e85c00bf3e46422b386472a32f58213eab
Seongwoo_Lukaid <crescent70004536@gmail.com>
2023-12-04 14:39:37 +0900

    feat: 방 채팅 스크롤 수정 (#137)

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -13,7 +13,7 @@ export default function Room() {
 
   return (
     <div className="flex items-center justify-center bg-fg">
-      <div className="flex max-h-screen w-full flex-col items-center gap-2 rounded-lg bg-bg p-4 shadow-2xl md:w-[70%] lg:w-[50%]">
+      <div className="flex h-screen w-full flex-col items-center gap-2 rounded-lg bg-bg p-4 shadow-2xl md:w-[70%] lg:w-[50%]">
         <RoomInfo roomCode={roomCode} />
         <Problems isHost={isHost} />
         <ScoreboardButton />
