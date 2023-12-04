7810db3d222f1ca1653deb4419dcbf435917356b
sjn0910 <sjn0910@naver.com>
2023-12-04 19:55:40 +0900

    feat: add string type on v

--- server/src/problem/dto/random.problem.dto.ts
+++ server/src/problem/dto/random.problem.dto.ts
@@ -2,12 +2,12 @@ import { Transform } from 'class-transformer';
 import { ArrayUnique, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class RandomProblemDto {
-  @Transform(({ value }) => value.split(',').map((v) => +v))
+  @Transform(({ value }) => value.split(',').map((v: string) => +v))
   @ArrayUnique()
   @IsNumber({}, { each: true })
   tagIds: number[];
 
-  @Transform(({ value }) => value.split(',').map((v) => +v))
+  @Transform(({ value }) => value.split(',').map((v: string) => +v))
   @ArrayUnique()
   @IsNumber({}, { each: true })
   levels: number[];
