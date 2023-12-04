7c6b0d909e321388436553a8bb3a9faf2db50cca
sjn0910 <sjn0910@naver.com>
2023-12-04 19:26:50 +0900

    feat: random problem dto

---
+++ server/src/problem/dto/random.problem.dto.ts
@@ -0,0 +1,19 @@
+import { Transform } from 'class-transformer';
+import { ArrayUnique, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class RandomProblemDto {
+  @Transform(({ value }) => value.split(',').map((v) => +v))
+  @ArrayUnique()
+  @IsNumber({}, { each: true })
+  tagIds: number[];
+
+  @Transform(({ value }) => value.split(',').map((v) => +v))
+  @ArrayUnique()
+  @IsNumber({}, { each: true })
+  levels: number[];
+
+  @Transform(({ value }) => +value)
+  @IsNumber()
+  @IsNotEmpty()
+  count: number;
+}
