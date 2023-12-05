18be4642b935715911c0da7fbf84813146b34927
dkubnt <kimdhyungg@gmail.com>
2023-12-05 13:41:01 +0900

    fix(joinRoom): joinroom api 수정

--- client/src/apis/joinRoom.ts
+++ client/src/apis/joinRoom.ts
@@ -4,8 +4,8 @@ const VITE_BASE_URL = import.meta.env.VITE_BASE_URL as string;
 
 export async function joinRoom(roomCode: string) {
   await axios.post(
-    `${VITE_BASE_URL}/room/${roomCode}`,
-    {},
+    `${VITE_BASE_URL}/room/join`,
+    { code: roomCode },
     {
       withCredentials: true,
     },
