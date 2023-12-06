9c302433b3af93b940714567f303bfc5182be8c3
Daehyun Kim <dqgthb@users.noreply.github.com>
2023-12-06 12:03:22 +0900

    roomCode typo to code (#155)

--- server/src/room/room.controller.ts
+++ server/src/room/room.controller.ts
@@ -24,7 +24,7 @@ export class RoomController {
     this.logger.debug(`user ${user.username} creating room...`);
     const room = await this.roomService.createRoom(user);
     this.logger.debug(`room: ${room.code} successfully created!!`);
-    return { roomCode: room.code };
+    return { code: room.code };
   }
 
   @Post('join')
