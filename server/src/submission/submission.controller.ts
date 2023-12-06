68e1765d82cff8ea7abb7add445e7d9cf6098977
sjn0910 <sjn0910@naver.com>
2023-12-06 11:16:07 +0900

    feat: submission 라우팅

--- server/src/submission/submission.controller.ts
+++ server/src/submission/submission.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SubmissionService } from './submission.service';
+import { SubmissionDto } from './dto/submission.dto';
 
 @Controller('submission')
-export class SubmissionController {}
+export class SubmissionController {
+  constructor(private readonly submissionService: SubmissionService) {}
+
+  @Post()
+  async submitCode(@Body() submissionDto: SubmissionDto) {
+    return await this.submissionService.submitCode(submissionDto);
+  }
+}
