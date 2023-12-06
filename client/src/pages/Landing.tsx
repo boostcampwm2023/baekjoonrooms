873c8adfe8b65387829b94a85d9a7deab196cb41
Seongwoo_Lukaid <crescent70004536@gmail.com>
2023-12-06 23:51:31 +0900

    feat: add video

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,6 +1,9 @@
 import { FaGithub } from 'react-icons/fa6';
+import VideoPlayer from '../components/VideoPlayer';
 
 export default function Landing() {
+  const videoSource = '/assets/test.mp4';
+
   return (
     <div className="h-full min-h-screen overscroll-none bg-bg pt-5">
       <header className="fixed z-50 w-full bg-opacity-50 shadow-lg backdrop-blur-sm transition duration-300 ease-in-out">
@@ -22,7 +25,7 @@ export default function Landing() {
       </header>
 
       <main className="flex flex-col items-center justify-center gap-y-16 pt-36">
-        <div className="text-transparent bg-gradient-to-r from-text_default to-default_white bg-clip-text text-center text-xl font-bold md:text-3xl lg:text-5xl">
+        <div className="text-transparent bg-gradient-to-r from-text_default to-gold bg-clip-text text-center text-xl font-bold md:text-3xl lg:text-5xl">
           Multiplayer rooms for BOJ
         </div>
         <a
@@ -38,7 +41,7 @@ export default function Landing() {
         </a>
 
         <div className="relative mx-8 lg:mx-28">
-          <div className="bg-gradient-radial from-yellow-700 absolute -inset-6 to-[hsl(0,0%,12%)] blur-[64px]"></div>
+          <div className="absolute -inset-6 rounded-xl border-[1px] bg-gradient-to-r from-gold to-gold opacity-20 blur-[64px]"></div>
           <img
             src="/assets/test.png"
             id="baekjoonrooms-screenshot"
@@ -46,23 +49,37 @@ export default function Landing() {
             className="border-slate-600 relative rounded-xl border-[1px]"
           />
         </div>
-        <div className="relative mx-8 lg:mx-28">
-          <div className="bg-gradient-radial from-yellow-700 absolute -inset-6 to-[hsl(0,0%,12%)] blur-[64px]"></div>
-          <img
-            src="/assets/test.png"
-            id="baekjoonrooms-screenshot"
-            alt="BaekjoonRooms screenshot"
-            className="border-slate-600 relative rounded-xl border-[1px]"
-          />
+
+        <div className="mb-14 mt-14 flex flex-row flex-wrap items-start justify-center gap-x-16 gap-y-12 px-28 text-text_default lg:gap-x-36">
+          <div className="flex flex-1 flex-col items-center gap-2 text-center">
+            <FaGithub size="4rem" className="text-text_default" />
+            <div className="font-bold">개쩌는 채팅</div>
+            <div className="text-md max-w-[16rem] opacity-60">
+              우리 채팅 ㄹㅇ 개쩜
+            </div>
+          </div>
+          <div className="flex flex-1 flex-col items-center gap-2 text-center">
+            <FaGithub size="4rem" className="text-text_default" />
+            <div className="font-bold">백준에 제출하면 자동 연동됨</div>
+            <div className="text-md max-w-[16rem] opacity-60">
+              백준에 제출하면 자동으로 연동됨 ㄹㅇ 개신기함
+            </div>
+          </div>
+          <div className="flex flex-1 flex-col items-center gap-2 text-center">
+            <FaGithub size="4rem" className="text-text_default" />
+            <div className="font-bold">프-라이버시</div>
+            <div className="text-md max-w-[16rem] opacity-60">
+              우리는 님들 프라이버시 존중해줌. 일단 채팅 저장 안하고, 코드도
+              싫다하면 안가져감 ㄹㅇ
+            </div>
+          </div>
         </div>
-        <div className="relative mx-8 lg:mx-28">
-          <div className="bg-gradient-radial from-yellow-700 absolute -inset-6 to-[hsl(0,0%,12%)] blur-[64px]"></div>
-          <img
-            src="/assets/test.png"
-            id="baekjoonrooms-screenshot"
-            alt="BaekjoonRooms screenshot"
-            className="border-slate-600 relative rounded-xl border-[1px]"
-          />
+
+        <div className="flex flex-col items-center justify-center gap-2 p-10">
+          <div className="text-transparent bg-gradient-to-r from-text_default to-gold bg-clip-text text-3xl font-bold">
+            시연영상 보시죠
+          </div>
+          <VideoPlayer videoSrc={videoSource} />
         </div>
       </main>
     </div>
