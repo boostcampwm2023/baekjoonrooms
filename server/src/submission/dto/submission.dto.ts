07b8447441e908cb3f548af9981fc97378e5330c
sjn0910 <sjn0910@naver.com>
2023-12-07 00:00:48 +0900

    feat: swagger 간단히 적용

--- server/src/submission/dto/submission.dto.ts
+++ server/src/submission/dto/submission.dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class SubmissionDto {
+  @ApiProperty({
+    example: '/status?user_id=ccp0209&problem_id=1001&from_mine=1',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   submitURL!: string;
 
+  @ApiProperty({
+    example: 'github',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   provider!: string;
 
+  @ApiProperty({
+    example: '12341234',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   providerId!: string;
--- server/src/submission/submission.controller.ts
+++ server/src/submission/submission.controller.ts
@@ -1,11 +1,17 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { SubmissionService } from './submission.service';
+import { ApiOperation } from '@nestjs/swagger';
 import { SubmissionDto } from './dto/submission.dto';
+import { SubmissionService } from './submission.service';
 
 @Controller('submission')
 export class SubmissionController {
   constructor(private readonly submissionService: SubmissionService) {}
 
+  @ApiOperation({
+    summary: '백준에서 코드가 제출됨',
+    description:
+      '백준에서 코드가 제출되었을 때 받는 요청을 처리하는 API로 채점이 완료될 때까지 작동합니다',
+  })
   @Post()
   async submitCode(@Body() submissionDto: SubmissionDto) {
     return await this.submissionService.submitCode(submissionDto);
