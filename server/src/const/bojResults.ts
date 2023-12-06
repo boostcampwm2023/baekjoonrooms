268f35cc9f308c8cfc838fa349a03c1dd5efc63d
sjn0910 <sjn0910@naver.com>
2023-12-06 10:56:58 +0900

    feat: 백준 문제 채점 결과

---
+++ server/src/const/bojResults.ts
@@ -0,0 +1,7 @@
+export const BojResults = {
+  waitings: ['wait', 'compile', 'judging'],
+  accepted: ['ac'],
+  partiallyAccepteds: ['pa', 'pac'],
+  wrongs: ['pe', 'tle', 'mle', 'ole', 'wa', 're', 'rte', 'ce'],
+  others: ['rejudge-wait', 'no-judge', 'co', 'del'],
+};
