21265c70a1d97b2653faa2acceec5a2197428f51
Kiwon Kim <74997112+kiuuon@users.noreply.github.com>
2023-12-06 13:44:10 +0900

    feat/extension-get-username (#158)
    * feat: 백준룸즈의 로컬스토리지에서 username, provider 받아와서 서버에 보내기
    
    * feat: 로그인 시 로컬스토리지에 userInfo 저장 / 로그아웃 시 삭제
    
    * feat: userInfo에서 username대신 providerId 받아오기
    
    * refactor: authContext -> authProvider

--- client/src/types/RoomCreateType.ts
+++ client/src/types/RoomCreateType.ts
@@ -1,13 +1,12 @@
-import { User } from "./UserType";
+import { User } from './UserType';
 
 export interface RoomCreateType {
-    code: string;
-    host: User;
-    users: User[];
-    endAt: string;
-    updatedAt: string;
-    deletedAt: string;
-    id: number;
-    createdAt: string;
-  }
-  
+  code: string;
+  host: User;
+  users: User[];
+  endAt: string;
+  updatedAt: string;
+  deletedAt: string;
+  id: number;
+  createdAt: string;
+}
