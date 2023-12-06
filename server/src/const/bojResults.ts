5820a8c5972550ad65abbebcc10477e609abcedb
sjn0910 <sjn0910@naver.com>
2023-12-06 23:44:16 +0900

    feat: 채점 트래킹 서비스

--- server/src/const/bojResults.ts
+++ server/src/const/bojResults.ts
@@ -7,6 +7,28 @@ export const BojResults = {
 } as const;
 
 export enum Status {
+  WAITING = 'WAITING',
   ACCEPTED = 'ACCEPTED',
   WRONG = 'WRONG',
 }
+
+export const BojResultsToStatus = {
+  wait: Status.WAITING,
+  compile: Status.WAITING,
+  judging: Status.WAITING,
+  ac: Status.ACCEPTED,
+  pa: Status.WRONG,
+  pac: Status.WRONG,
+  pe: Status.WRONG,
+  tle: Status.WRONG,
+  mle: Status.WRONG,
+  ole: Status.WRONG,
+  wa: Status.WRONG,
+  re: Status.WRONG,
+  rte: Status.WRONG,
+  ce: Status.WRONG,
+  'rejudge-wait': Status.WRONG,
+  'no-judge': Status.WRONG,
+  co: Status.WRONG,
+  del: Status.WRONG,
+};
--- server/src/submission/submission.service.ts
+++ server/src/submission/submission.service.ts
@@ -1,11 +1,14 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as cheerio from 'cheerio';
+import { BojResultsToStatus, Status } from 'src/const/bojResults';
 import Submission from 'src/entities/submission.entity';
 import { ProblemService } from 'src/problem/problem.service';
+import { RoomUserService } from 'src/roomUser/room.user.service';
+import { BojSubmissionInfo } from 'src/types/submission';
 import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
 import { SubmissionDto } from './dto/submission.dto';
-import { BojSubmissionInfo } from 'src/types/submission';
 
 @Injectable()
 export class SubmissionService {
@@ -15,6 +18,7 @@ export class SubmissionService {
 
     private readonly userService: UserService,
     private readonly problemService: ProblemService,
+    private readonly roomUserService: RoomUserService,
   ) {}
 
   async submitCode(submissionDto: SubmissionDto) {
@@ -23,12 +27,7 @@ export class SubmissionService {
       .split('?')[1]
       .split('&')
       .map((v) => v.split('=')[1]);
-
-    const bojSubmissionInfo: BojSubmissionInfo = {
-      bojUserId,
-      bojProblemStringId,
-      submittedAt: new Date(),
-    };
+    const submittedAt = new Date(); // extension으로 sumbit time을 받아서 판단하는 것이 더 정확한데 현재는 그냥 request가 들어오는 시간을 submittedAt으로 사용함
     const bojProblemId = Number(bojProblemStringId);
     const { provider, providerId } = submissionDto;
 
@@ -38,24 +37,86 @@ export class SubmissionService {
     });
     if (!user) throw new BadRequestException('존재하지 않는 유저입니다.');
 
-    if (!user.joinedRooms || user.joinedRooms.length === 0)
-      throw new BadRequestException('참여중인 방이 없습니다.');
-    const room = user.joinedRooms[0];
+    const roomUser =
+      await this.roomUserService.findRoomUserByUserWithRoom(user);
+    if (!roomUser) throw new BadRequestException('참여중인 방이 없습니다.');
+    const room = roomUser.room;
 
     const problem =
       await this.problemService.getProblemByBojProblemId(bojProblemId);
     if (!problem) throw new BadRequestException('존재하지 않는 문제입니다.');
 
-    const status = await this.getBojSubmissionStatus(bojSubmissionInfo);
+    // TODO : 이 즈음에서 socket emit 해서 클라이언트에서 제출한 문제가 제출되었다는 것을 알려줘야 함
 
-    return await this.submissionRepository.create({
-      status,
-      user,
-      room,
-      problem,
-      submittedAt: bojSubmissionInfo.submittedAt,
+    const status = await this.getBojSubmissionStatus({
+      bojUserId,
+      bojProblemStringId,
+      submittedAt,
     });
+
+    // TODO : 이 즈음에서 socket emit 해서 클라이언트에서 제출한 문제가 채점된 것을 알려줘야 함
+
+    return await this.submissionRepository
+      .create({
+        status,
+        user,
+        room,
+        problem,
+        submittedAt,
+      })
+      .save();
   }
 
-  async getBojSubmissionStatus(bojSubmissionInfo: BojSubmissionInfo) {}
+  // TODO : extension으로 sumbit time을 받지 않기에 현재는 제일 처음에 fetch 했을 때, 가장 위에 있는 row를 제출했던 것으로 간주함
+  async getBojSubmissionStatus(bojSubmissionInfo: BojSubmissionInfo) {
+    let status = Status.WAITING;
+    let bojSolutionId: string = '';
+    const { bojUserId, bojProblemStringId } = bojSubmissionInfo;
+    const bojStatusUrl = `https://www.acmicpc.net//status?user_id=${bojUserId}&problem_id=${bojProblemStringId}`;
+
+    while (status === Status.WAITING) {
+      const statusPage = await fetch(bojStatusUrl).then((res) => res.text());
+      const $ = cheerio.load(statusPage);
+      const allSubmissions = $('tbody > tr');
+
+      allSubmissions.each((index, element) => {
+        const { tmpBojSolutionId, tmpStatus } = this.getEachSubmissionInfo(
+          $,
+          element,
+        );
+
+        if (bojSolutionId === '') {
+          bojSolutionId = tmpBojSolutionId;
+          status = BojResultsToStatus[tmpStatus];
+          return false;
+        }
+
+        if (tmpBojSolutionId === bojSolutionId) {
+          status = BojResultsToStatus[tmpStatus];
+          return false;
+        }
+      });
+
+      if (status === Status.WAITING) await this.sleep(500);
+    }
+
+    return status;
+  }
+
+  getEachSubmissionInfo(
+    $: cheerio.CheerioAPI,
+    submissionElement: cheerio.Element,
+  ) {
+    const tdElements = $(submissionElement).find('td');
+    const tmpBojSolutionId = $(tdElements[0]).text();
+    const tmpStatus = $(tdElements[3])
+      .find('span')
+      .attr('data-color') as string;
+
+    return { tmpBojSolutionId, tmpStatus };
+  }
+
+  async sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
