987038f233bd6eca09472e9af91cd08f996dde12
kiuuon <k41471804@gmail.com>
2023-12-09 15:55:44 +0900

    refactor: 방에 들어가고 나갈 때 활성화/비활성화 되는 로직 수정 및 remote용, local용 url 나눠서 주석처리해놓기

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -5,14 +5,14 @@ const targetNode = document.body;
 const config = { childList: true, subtree: true };
 
 observer.observe(targetNode, config);
-
 function injectScript() {
-  const urlPattern = /^http:\/\/localhost:5173\/room\/.*$/;
+  // const urlPattern = /^http:\/\/localhost:5173\/room\/.*$/;
+  const urlPattern = /^https:\/\/baekjoonrooms.com\/room\/.*$/;
   if (urlPattern.test(window.location.href) && !isRoom) {
     const userInfo = JSON.parse(localStorage.getItem('userInfo'));
     chrome.runtime.sendMessage({ isActive: true, userInfo });
     isRoom = true;
-  } else {
+  } else if (!urlPattern.test(window.location.href) && isRoom) {
     chrome.runtime.sendMessage({ isActive: false });
     isRoom = false;
   }
