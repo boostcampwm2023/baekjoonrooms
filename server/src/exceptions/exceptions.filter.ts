aa8a5b81785d24a5c699748f04b24551a4180d46
Daehyun Kim <kimdhyungg@gmail.com>
2023-12-10 16:43:09 +0900

    feat(exceptionsFilter): BadRequest, Forbidden 익셉션은 warn 처리

--- server/src/exceptions/exceptions.filter.ts
+++ server/src/exceptions/exceptions.filter.ts
@@ -1,5 +1,13 @@
-import { ArgumentsHost, Catch, HttpException, Logger } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  BadRequestException,
+  Catch,
+  ForbiddenException,
+  HttpException,
+  Logger,
+} from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
+import * as util from 'util';
 
 @Catch()
 export class ExceptionsFilter extends BaseExceptionFilter {
@@ -9,11 +17,23 @@ export class ExceptionsFilter extends BaseExceptionFilter {
     if (exception instanceof HttpException) {
       const status = exception.getStatus();
       const { name, message, stack } = exception;
-
-      this.logger.error(
-        `Server raises ${name}: ${status} Error Message: ${message}`,
-        stack,
-      );
+      if (
+        exception instanceof BadRequestException ||
+        exception instanceof ForbiddenException
+      ) {
+        this.logger.warn(`Server raises ${name}: ${status}: ${message}`, stack);
+      } else {
+        this.logger.error(
+          `Server raises HttpException ${name}: ${status}: ${message}`,
+          stack,
+        );
+      }
+    } else if (exception instanceof Error) {
+      const { name, message, stack } = exception;
+      this.logger.error(`Server raises Error ${name}: ${message}`, stack);
+    } else {
+      this.logger.error(`Server raises an unknown exception...`);
+      this.logger.error(util.inspect(exception));
     }
 
     super.catch(exception, host);
