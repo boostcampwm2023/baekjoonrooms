2c8a4e45ed3fd71ec37d5cb10c099e517386c111
Daehyun Kim <kimdhyungg@gmail.com>
2023-12-16 03:07:05 +0900

    style(eslint): :art: eqeqeq, no-implicit-coercion

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    eqeqeq: ['error', 'always'],
+    'no-implicit-coercion': ['error', { allow: ['!!'] }],
   },
 };
