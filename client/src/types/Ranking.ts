464c329649486a4fa9f308ad78470b4d5a5e0a18
Yechan Jung <asdxcv1998@gmail.com>
2024-02-07 23:22:32 +0900

    refactor: 스코어보드 리팩토링

--- client/src/types/Ranking.ts
+++ client/src/types/Ranking.ts
@@ -2,5 +2,5 @@ export interface Ranking {
   id: number;
   username: string;
   numberOfProblemsSolved: number;
-  mostRecentCorrectSubmissionTime: string;
+  mostRecentCorrectSubmissionTime: string | null; // is nullable?
 }
