6b7d6014c5f719ee34afc18db32a2649a8ab668f
Daehyun Kim <kimdhyungg@gmail.com>
2024-02-10 04:45:04 +0900

    merge main

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-unused-vars': [
+    '@typescript-eslint/no-unused-vars': [
       'error',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
     ],
