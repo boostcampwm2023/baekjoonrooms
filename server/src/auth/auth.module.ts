7134ee25bc5f720d2cac8b537db137e34a3ea1c2
Daehyun Kim <kimdhyungg@gmail.com>
2024-02-11 08:38:14 +0900

    move passport to auth guard for better modularity

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,26 +1,22 @@
 import { Module } from '@nestjs/common';
-import { GithubStrategy, MockStrategy } from './auth.strategy';
-import { AuthController } from './auth.controller';
-import { LocalSerializer } from './auth.serializer';
-import { UserModule } from '../user/user.module';
-import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '../user/user.module';
+import { AuthController } from './auth.controller';
 import { GithubAuthGuard, MockAuthGuard } from './auth.guard';
+import { LocalSerializer } from './auth.serializer';
+import { AuthService } from './auth.service';
+import { GithubStrategy, MockStrategy } from './auth.strategy';
 
 @Module({
   providers: [
     GithubStrategy,
     GithubAuthGuard,
     MockAuthGuard,
-    // MockAuthGuard,
-
-    // SessionAuthGuard,
     MockStrategy,
-
     AuthService,
     LocalSerializer,
   ],
   controllers: [AuthController],
-  imports: [UserModule, PassportModule],
+  imports: [UserModule, PassportModule.register({ session: true })],
 })
 export class AuthModule {}
