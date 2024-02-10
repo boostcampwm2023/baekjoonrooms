ba45527d6bed49560da50edaaf17f4e81c02ca0c
Daehyun Kim <kimdhyungg@gmail.com>
2024-02-11 08:43:51 +0900

    test(!): write a working e2e test for the project for automation!

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,24 +1,45 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
+import Redis from 'ioredis';
+import {
+  makeRedisStore,
+  makeSessionMiddleware,
+} from 'src/common/middleware/session';
+import { CustomLogger } from 'src/logger/custom.logger';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
+
+Error.stackTraceLimit = Infinity;
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let redisClient: Redis;
+  const logger = new CustomLogger();
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    const { redis, redisStore } = makeRedisStore();
+    const sessionMiddleware = makeSessionMiddleware(redisStore);
+    redisClient = redis;
+    app.use(sessionMiddleware);
+
+    app.useLogger(logger);
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+    await redisClient.quit();
+  });
+
+  it('/ (GET)', async () => {
+    logger.log('test');
+    return request(app.getHttpServer()).get('/').expect(403);
   });
 });
